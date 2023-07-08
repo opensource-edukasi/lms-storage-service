c675af92c4786682248af7cf7c4c8436338704e5
teguh2910 <teguh.yuhono@student.unsia.ac.id>
2023-07-09 01:50:02 +0700

    implement env port on server.go

--- skeleton/server.go
+++ skeleton/server.go
@@ -1,68 +1,70 @@
 package main
 
 import (
-    "context"
-    "log"
-    "net"
-    "os"
-    "skeleton/pb/drivers"
-    "skeleton/pb/generic"
+	"context"
+	"log"
+	"net"
+	"os"
+	"skeleton/config"
+	"skeleton/pb/drivers"
+	"skeleton/pb/generic"
 
-    "google.golang.org/grpc"
+	"google.golang.org/grpc"
 )
 
 func main() {
+	config.Setup(".env")
 
-    log := log.New(os.Stdout, "grpc skeleton : ", log.LstdFlags|log.Lmicroseconds|log.Lshortfile)
+	log := log.New(os.Stdout, "grpc skeleton : ", log.LstdFlags|log.Lmicroseconds|log.Lshortfile)
 
-    // listen tcp port
-    lis, err := net.Listen("tcp", ":7070")
-    if err != nil {
-        log.Fatalf("failed to listen: %v", err)
-        return
-    }
+	// listen tcp port
+	lis, err := net.Listen("tcp", ":"+os.Getenv("PORT"))
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+		return
+	}
 
-    grpcServer := grpc.NewServer()
+	grpcServer := grpc.NewServer()
 
-    // routing grpc services
-    grpcRoute(grpcServer, log)
+	// routing grpc services
+	grpcRoute(grpcServer, log)
 
-    if err := grpcServer.Serve(lis); err != nil {
-        log.Fatalf("failed to serve: %s", err)
-        return
-    }
-    log.Print("serve grpc on port: 7070")
+	if err := grpcServer.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %s", err)
+		return
+	}
+	log.Print("serve grpc on port:" + os.Getenv("PORT"))
 
 }
 
 func grpcRoute(grpcServer *grpc.Server, log *log.Logger) {
-    driverServer := newDriverHandler(log)
+	driverServer := newDriverHandler(log)
 
-    drivers.RegisterDriversServiceServer(grpcServer, driverServer)
+	drivers.RegisterDriversServiceServer(grpcServer, driverServer)
 }
 
 type driverHandler struct {
-    log *log.Logger
+	log *log.Logger
 }
 
 func newDriverHandler(log *log.Logger) *driverHandler {
-    handler := new(driverHandler)
-    handler.log = log
-    return handler
+	handler := new(driverHandler)
+	handler.log = log
+	return handler
 }
 
 func (u *driverHandler) List(ctx context.Context, in *drivers.DriverListInput) (*drivers.Drivers, error) {
-    return &drivers.Drivers{}, nil
+	return &drivers.Drivers{}, nil
 }
 
 func (u *driverHandler) Create(ctx context.Context, in *drivers.Driver) (*drivers.Driver, error) {
-    return in, nil
+	return in, nil
 }
 
 func (u *driverHandler) Update(ctx context.Context, in *drivers.Driver) (*drivers.Driver, error) {
-    return in, nil
+	return in, nil
 }
 
 func (u *driverHandler) Delete(ctx context.Context, in *generic.Id) (*generic.BoolMessage, error) {
-    return &generic.BoolMessage{}, nil
+	return &generic.BoolMessage{}, nil
 }
